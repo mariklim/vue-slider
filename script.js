f856e1feabba28227c67451b82558d1f70766736
mariklim <maria.klimenko889@gmail.com>
2021-09-17 16:03:47 +0200

    add html and css base

---
+++ script.js
@@ -0,0 +1,15 @@
+// https://www.freepik.com/free-photo/homemade-newyork-cheesecake-with-frozen-berries-mint-healthy-organic-dessert-top-view_8886135.htm#page=1&query=cheese%20cake&position=16
+
+// https://www.freepik.com/free-photo/front-view-delicious-round-pie-sweet-bake-dark-background-biscuit-dough-pie-cake-sugar-sweet-tea_16907804.htm#page=1&query=pumpkin%20cake&position=0
+
+//https://www.freepik.com/free-photo/homemade-delicious-apple-pie-with-jam_15332245.htm#page=1&query=apple%20cake&position=2
+
+const app = new Vue({
+    el: '#root',
+    data: {
+      
+    },
+    methods:{
+       
+    },
+  })
