538511a75596426b118b1dcd0b06d53b85256881
mariklim <maria.klimenko889@gmail.com>
2021-09-17 18:37:05 +0200

    add images

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const app = new Vue({
         images: [
             "https://image.freepik.com/free-photo/homemade-newyork-cheesecake-with-frozen-berries-mint-healthy-organic-dessert-top-view_114579-9844.jpg",
             "https://image.freepik.com/free-photo/front-view-delicious-round-pie-sweet-bake-dark-background-biscuit-dough-pie-cake-sugar-sweet-tea_140725-115655.jpg",
-            "https://image.freepik.com/free-photo/front-view-yummy-apple-pie-sweet-baked-inside-pan-white-desk-pie-cake-biscuit-sweet-sugar-bake_140725-39947.jpg"
+            "https://image.freepik.com/free-photo/front-view-yummy-apple-pie-sweet-baked-inside-pan-white-desk-pie-cake-biscuit-sweet-sugar-bake_140725-39947.jpg",
+            "https://image.freepik.com/free-photo/carrot-cake-decorated-with-berries-figs-white-plate-traditional-donkey-pastries_127032-1897.jpg",
+            "https://img.freepik.com/free-photo/close-up-candycane-lollipop-colorful-sweets-background_53876-31923.jpg?size=338&ext=jpg",
+            "https://image.freepik.com/free-photo/front-view-sweet-pancakes-tower-arrangement_23-2148654085.jpg",
         ],
         indexImg: 0,
 
